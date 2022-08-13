33858011e0c337f9ec78a1ef21d227dc1b287219
saptak das <dassaptak0@gmail.com>
2022-08-13 16:44:20 +0530

    File added...

---
+++ 8.Pangram.java
@@ -0,0 +1,24 @@
+package com.saptak;
+
+public class Pangram {
+
+    public static boolean pangram(String sentence)
+    {
+        boolean[] ans = new boolean[26];
+        for(int i=0;i<sentence.length();i++)
+        {
+            ans[sentence.charAt(i) -97] = true;
+        }
+        for(int j=0;j<26;j++)
+        {
+            if(ans[j] == false) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        boolean result = pangram(sentence);
+        System.out.println(result);
+    }
+}
