350d769cbe421abe7a8db3e1dd3db93e2c5ee40a
saptak das <dassaptak0@gmail.com>
2022-08-19 11:50:08 +0530

    File added...

---
+++ 028.Reach_a_number.java
@@ -0,0 +1,26 @@
+package com.saptak;
+
+public class Reach_a_number {
+    public static int helper(int target)
+    {
+        target = Math.abs(target);
+        int sum = 0, steps = 0;
+        while(sum < target)
+        {
+            sum += steps;
+            steps++;
+        }
+        while((sum - target) % 2 == 1)
+        {
+            sum += steps;
+            steps++;
+        }
+        return steps -1;
+    }
+
+    public static void main(String[] args) {
+        int target = 2;
+        int ans = helper(target);
+        System.out.println(ans);
+    }
+}
