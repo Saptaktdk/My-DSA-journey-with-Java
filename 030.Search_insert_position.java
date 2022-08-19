26363aab8080451656d09cb2981bd7bf9ff94114
saptak das <dassaptak0@gmail.com>
2022-08-19 12:25:17 +0530

    File added...

---
+++ 030.Search_insert_position.java
@@ -0,0 +1,41 @@
+package com.saptak;
+
+//problem link: https://leetcode.com/problems/search-insert-position/
+
+
+public class Search_insert_position {
+
+    public static int helper(int[] nums, int target)
+    {
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            if(nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else if(nums[mid] > target)
+            {
+                end = mid -1;
+            }
+            else
+            {
+                 return  mid;
+            }
+
+
+
+        }
+        return start;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 2;
+        int ans = helper(nums, target);
+        System.out.println(ans);
+    }
+}
