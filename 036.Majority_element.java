e5d44e59721a75092c05fdfbc4968646a17f99a1
saptak das <dassaptak0@gmail.com>
2022-08-22 09:51:41 +0530

    File Added...

---
+++ 036.Majority_element.java
@@ -0,0 +1,32 @@
+package com.saptak;
+
+import java.util.HashMap;
+
+public class Majority_element {
+    public static int helper(int[] nums)
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num: nums)
+        {
+            if(map.containsKey(num))
+            {
+                map.put(num, map.get(num)+1);
+            }
+            else
+            {
+                map.put(num,1);
+                if(map.get(num) > nums.length/2)
+                {
+                    return num;
+                }
+            }
+        }
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,3};
+        int ans = helper(nums);
+        System.out.println(ans);
+    }
+}
