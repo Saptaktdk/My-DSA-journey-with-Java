f451e982fbf6d54c4de53047b68c0f2cb59d7a1b
saptak das <dassaptak0@gmail.com>
2022-08-24 11:36:24 +0530

    File added...

---
+++ 050.Power_of_two.java
@@ -0,0 +1,21 @@
+package com.saptak;
+
+public class Power_of_two {
+    public static boolean helper(int n)
+    {
+        //base case:
+        if(n==1) return true;
+        int i=1;
+        while(i<n)
+        {
+            i *= 2;
+        }
+        return (i==n);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        boolean ans = helper(n);
+        System.out.print(ans);
+    }
+}
