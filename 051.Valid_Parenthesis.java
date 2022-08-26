8d41fc8e83621aa5348857a5100a1fbd99d61d6c
saptak das <dassaptak0@gmail.com>
2022-08-26 11:04:47 +0530

    File added...

---
+++ 051.Valid_Parenthesis.java
@@ -0,0 +1,39 @@
+package com.saptak;
+
+import java.util.Stack;
+
+public class Valid_Parenthesis {
+    public static boolean helper(String s)
+    {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char x = s.charAt(i);
+            if(x == '(' || x == '{' || x == '[')
+            {
+                st.push(x);
+            }
+
+            else if(st.empty()) return false;
+            else if(
+                    (x == ')' && st.pop() != '(') ||
+                            (x == '}' && st.pop() != '{') ||
+                            (x == ']' && st.pop() != '[')
+            )
+            {
+                return false;
+            }
+
+
+
+
+        }
+        return st.empty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        boolean ans = helper(s);
+        System.out.print(ans);
+    }
+}
