d9ed876a422db6c2a66893f6dd945fc95ace2e5e
saptak das <dassaptak0@gmail.com>
2022-08-29 13:30:54 +0530

    File added...

---
+++ 057.Add_Binary.java
@@ -0,0 +1,32 @@
+package com.saptak;
+
+public class Add_Binary {
+    public static String helper(String a, String b)
+    {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int sum, carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i>=0 || j>=0)
+        {
+            sum = carry;
+            if(i>=0) sum += a.charAt(i) - '0'; //converting the character to integer
+            if(j>=0) sum += b.charAt(j) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+            i--;
+            j--;
+
+        }
+        if(carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+
+    }
+
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+        String ans = helper(a,b);
+        System.out.print(ans);
+    }
+}
