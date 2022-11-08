f71e344a694562b24d6b24b84eb56bb3fe3737bf
Saptaktdk <dassaptak0@gmail.com>
2022-11-08 08:20:25 +0000

    Added :tada:

---
+++ 073.Check_if_one_word_is_a_substring_of_other.java
@@ -0,0 +1,22 @@
+package com.saptak;
+
+public class Check_if_one_word_is_a_substring_of_other {
+    public static void isRotation(String s1, String s2)
+    {
+        s1 = s1.concat(s1);
+        isSubstring(s1,s2);
+    }
+
+    //function to check if s2 is a substring of s1
+    public static void isSubstring(String s1, String s2)
+    {
+        boolean res = s1.contains(s2);
+        System.out.print(res);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "waterbottle";
+        String s2 = "erbottlewat";
+        isRotation(s1,s2);
+    }
+}
