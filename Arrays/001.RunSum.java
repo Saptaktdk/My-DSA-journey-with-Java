914fa879ca53ff9fcb31d46756640e32bd4ffd91
Saptak Das <dassaptak0@gmail.com>
2022-12-09 02:09:34 +0530

    Update 001.RunSum.java

--- Arrays/001.RunSum.java
+++ Arrays/001.RunSum.java
@@ -1,5 +1,13 @@
 package com.saptak;
+/*Problem link: https://leetcode.com/problems/running-sum-of-1d-array/
 
+Description: Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running sum
+
+Input examples:
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as folows: [1, 1+2, 1+2+3, 1+2+3+4].
+Return the running sum of nums. */
 
 public class RunSum {
 
