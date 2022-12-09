b7dfa03b39de65270d01fee92578ecb54de6d172
Saptak Das <dassaptak0@gmail.com>
2022-12-09 18:40:51 +0530

    Update 003.Richest_customer_wealth.java

--- Arrays/003.Richest_customer_wealth.java
+++ Arrays/003.Richest_customer_wealth.java
@@ -1,5 +1,20 @@
 package com.saptak;
 
+/*problem link: https://leetcode.com/problems/richest-customer-wealth/
+
+Description: You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the ith customer has in the jth bank.
+Return the wealth that the richest customer has.
+A customer's wealth is the amount of money they have in all their bank accounts.
+The richest customer is the customer that has the maximum wealth.
+
+Input: accounts = [[1,5],[7,3],[3,5]]
+Output: 10
+Explanation: 
+1st customer has wealth = 6
+2nd customer has wealth = 10 
+3rd customer has wealth = 8
+The 2nd customer is the richest with a wealth of 10.*/
+
 public class Richest_customer_wealth {
     public static void main(String[] args) {
         int[][] accounts = {{1,2,3},{3,2,1}};
