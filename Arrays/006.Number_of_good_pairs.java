3bd630a2a43a9b04171cf8d36f44bbfcaac7ca26
Saptak Das <dassaptak0@gmail.com>
2022-12-09 18:51:37 +0530

    Update 006.Number_of_good_pairs.java

--- Arrays/006.Number_of_good_pairs.java
+++ Arrays/006.Number_of_good_pairs.java
@@ -1,6 +1,14 @@
 package com.saptak;
 
-//problem link: https://leetcode.com/problems/number-of-good-pairs/
+/*problem link: https://leetcode.com/problems/number-of-good-pairs/
+
+Description: Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.*/
 
 public class Number_of_good_pairs {
     public static int pairs(int[] nums)
