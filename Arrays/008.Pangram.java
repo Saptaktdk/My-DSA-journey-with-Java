0895301a9099b08f0595e0ec351ab54a133dc190
Saptak Das <saptakdev123@gmail.com>
2022-12-09 18:54:58 +0530

    Update 008.Pangram.java

--- Arrays/008.Pangram.java
+++ Arrays/008.Pangram.java
@@ -1,6 +1,14 @@
 package com.saptak;
 
-//problem link: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+/*problem link: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+Description: A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.*/
 
 public class Pangram {
 
