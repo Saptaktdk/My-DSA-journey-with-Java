07f2a7333e4361a239062c5bf99c5b0bbe2e6e9a
Saptak Das <saptakdev123@gmail.com>
2022-12-09 18:58:41 +0530

    Update 010.Maximum_Subarray.java

--- Arrays/010.Maximum_Subarray.java
+++ Arrays/010.Maximum_Subarray.java
@@ -1,6 +1,12 @@
 package com.saptak;
 
-//problem link: https://leetcode.com/problems/maximum-subarray/
+/*problem link: https://leetcode.com/problems/maximum-subarray/
+
+Description: Given an integer array nums, find the subarray which has the largest sum and return its sum.
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.*/
 
 public class Maximum_Subarray {
     public static int max_subarray(int[] nums)
